af1dc11e97bed8bffd4f258b429f01cc279a0d08
RobbieNesmith <bobberto1995@gmail.com>
2017-04-07 01:53:59 +0000

    adds temperature sensor

--- stalker.py
+++ stalker.py
@@ -6,6 +6,7 @@ from functools import wraps, update_wrapper
 from datetime import datetime
 import os
 import time
+import dht11
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -13,15 +14,28 @@ GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 app = Flask(__name__)
 app.debug = True
 
+temp = 0
+humidity = 0
+
 @app.route("/")
 def index():
   fname = take_picture()
   lights = get_lights_on()
+  th = get_temp()
   lstr = "off"
   if lights:
     lstr = "on"
-  return render_template("index.html",lights=lstr,fname=fname)
+  return render_template("index.html",lights=lstr,fname=fname,temp=th[0],humidity=th[1])
 
+def get_temp():
+  global temp
+  global humidity
+  instance = dht11.DHT11(pin = 14)
+  result = instance.read()
+  if result.is_valid():
+    temp = result.temperature
+    humidity = result.humidity
+  return ("Temperature {}\N{DEGREE SIGN}C, {}\N{DEGREE SIGN}F".format(temp,temp * 9/5 + 32),"Humidity {}%".format(humidity))
 
 def take_picture():
   fname = str(int(time.time()))
